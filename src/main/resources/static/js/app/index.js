7c826dba43a1154088d0b0ce5e92ed00185a11b8
girl <kchg2613@naver.com>
2022-10-03 14:45:49 +0900

    삭제 기능 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -8,6 +8,10 @@ var main = {
         $('#btn-update').on('click', function() {
             _this.update();
         })
+
+        $('#btn-delete').on('click', function() {
+            _this.delete();
+        })
     },
     save : function(){
         var data = {
@@ -42,14 +46,30 @@ var main = {
             url : '/api/v1/posts/'+id,
             dataType : 'json',
             contentType : 'application/json; charset=utf-8',
-            data : JSON.strigify(data)
+            data : JSON.stringify(data)
         }).done(function(){
             alert('글이 수정되었습니다.');
             window.location.href = '/';
         }).fail(function(error){
             alert(JSON.stringify(error));
         })
-    }
+    },
+     delete : function(){
+         var id = $('#id').val();
+
+         $.ajax({
+             type : 'DELETE',
+             url : '/api/v1/posts/'+id,
+             dataType : 'json',
+             contentType : 'application/json; charset=utf-8',
+             data : JSON.stringify(data)
+         }).done(function(){
+             alert('글이 삭제되었습니다.');
+             window.location.href = '/';
+         }).fail(function(error){
+             alert(JSON.stringify(error));
+         })
+     }
 };
 
 main.init();
